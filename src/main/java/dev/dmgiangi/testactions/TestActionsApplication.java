007eb76dc1162343bb4d4887f3c74067f5cb8ae2
Gianluigi De Marco <dmgiangi@gmail.com>
2022-10-06 19:47:05 +0200

    create a spring empty project

---
+++ src/main/java/dev/dmgiangi/testactions/TestActionsApplication.java
@@ -0,0 +1,13 @@
+package dev.dmgiangi.testactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestActionsApplication.class, args);
+	}
+
+}
