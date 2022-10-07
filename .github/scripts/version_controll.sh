0504692130374454f2a017a917bbf2a3738ae85d
Gianluigi De Marco <dmgiangi@gmail.com>
2022-10-07 21:51:18 +0200

    add version control script

---
+++ .github/scripts/version_controll.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd .
+date
+PR_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }')
+
+echo
+echo "- Master version:       $MASTER_VERSION"
+echo "- pull request version: $PR_VERSION"
+echo
+
+if [ "$MASTER_VERSION" = "$PR_VERSION" ]
+then
+	echo "::error::ERROR - The pom.xml file of the pull request have the same version of the master branch"
+	#exit 1
+else
+	if [  "$MASTER_VERSION" = "`echo -e "$MASTER_VERSION\n$PR_VERSION" | sort -V | head -n1`" ]
+	then
+         	echo "OK - the version of the pullrequest is greater than the version in the main branch"
+        else
+        	echo "::error::ERROR - The version in the pom.xml of the pull request is less of the version of the master branch"
+        	#exit 1
+        fi
+fi
+
+echo
+
+DISCORDING_VERSION=$(grep -rnw . -e '* @version' | grep -v '@version $PR_VERSION')
+          
+if [ -z "${DISCORDING_VERSION-unset}" ]
+then
+	echo "OK - The version in the javado is valid"
+else
+	echo "ERROR - the sequent file have a invalid versione reported:"
+	grep -rnw . -e '* @version' | grep -v '@version $PR_VERSION'
+	echo "::error::ERROR - Some file have wrong version reported"
+        	#exit 1
+fi
