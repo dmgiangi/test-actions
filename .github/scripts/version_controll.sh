bf2eb6d626fec0cd56b1648e1efd41d72cf581a8
dmgiangi <76408605+dmgiangi@users.noreply.github.com>
2022-10-08 10:32:22 +0200

    Update version_controll.sh

--- .github/scripts/version_controll.sh
+++ .github/scripts/version_controll.sh
@@ -33,7 +33,7 @@ then
 else
 	echo "ERROR - the sequent file have a invalid version reported:"
 	T=$(grep -rnE "\* @version " ./src)
-	[ ! -e $T ] && {grep -v "@version $PR_VERSION" <<< $T}
+	grep -v "@version $PR_VERSION" <<< $T
 	echo " "
 	echo "::error::ERROR - Some file have wrong version reported"
 	FAIL=1
